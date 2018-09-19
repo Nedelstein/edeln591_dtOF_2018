ec39eea2163d7d8af0444bbb2ca3e7c250d1e9be
Noah Edelstein <Noedelstein@gmail.com>
2018-09-18 20:24:54 -0400

    w03_h02_selfPortrait git commit -m w03_h02_selfPortrait:

---
+++ w03_h02_selfPortrait/src/Face.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "ofMain.h"
+
+class Face {
+
+public:
+void setup();
+void update();
+void draw();
+    void mousePressed(int x, int y, int button);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+
+
+
+    ofImage leftEye;
+    ofImage rightEye;
+    ofImage mouth;
+    ofImage nose;
+    
+    float mouthX;
+    float mouthY;
+    float leftEyeX;
+    float leftEyeY;
+    float rightEyeX;
+    float rightEyeY;
+    float noseX;
+    float noseY;
+    
+    float noseSpeed;
+    float mouthSpeed;
+    float leftEyeSpeed;
+    float rightEyeSpeed;
+    
+    glm::vec2 pos;
+    
+    glm::vec2 leftEyeVec;
+    glm::vec2 rightEyeVec;
+    glm::vec2 mouthVec;
+    glm::vec2 noseVec;
+    
+        
+};
+
---
+++ w03_h02_selfPortrait/src/ofApp.cpp
@@ -0,0 +1,105 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetBackgroundColor(0);
+    for(int i=0; i<1; i++){
+        face[i].setup();
+    }
+//    nose.load("nose.png");
+//    leftEye.load("leftEye.png");
+//    rightEye.load("rightEye.png");
+//    mouth.load("mouth.png");
+//
+//     noseX = ofGetWidth()/2;
+//     noseY = ofGetHeight()/2;
+//      leftEyeX = noseX-100;
+//     leftEyeY = noseY-100;
+//    rightEyeX = noseX +100;
+//    rightEyeY = leftEyeY;
+//    mouthX = noseX- 40;
+//    mouthY = noseY + 200;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+      for(int i=0; i<1; i++){
+          face[i].update();
+      }
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    for(int i=0; i<1; i++){
+        face[i].draw();
+    }
+    
+//    nose.draw(noseX, noseY);
+//    leftEye.draw(leftEyeX, leftEyeY);
+//    rightEye.draw(rightEyeX, rightEyeY);
+//    mouth.draw(mouthX, mouthY);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    for(int i=0; i<1; i++){
+        face[i].mouseDragged(x,y,button);
+    }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+//        for(int i=0; i<1; i++){
+//            face[i].mousePressed(x,y,button);
+//        }
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
